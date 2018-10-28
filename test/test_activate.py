1d49864aad0df5e3e767e10c442cb37514df6dad
SelaDanti <kwameasiago@gmail.com>
2018-10-28 22:35:12 +0300

    :new: [Feature #161530871] Set up verifications

--- test/test_activate.py
+++ test/test_activate.py
@@ -25,42 +25,55 @@ class TestActivate(unittest.TestCase):
 		self.data['first name'] = ''
 		res = post(self.test,self.url,self.data,self.content_type)
 		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_whitespace_data(self):
 		self.data['first name'] = '   '
 		res = post(self.test,self.url,self.data,self.content_type)
 		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_not_email(self):
 		self.data['email'] = 'notmail'
 		res = post(self.test,self.url,self.data,self.content_type)
 		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_not_activation_key(self):
 		self.data['activation key'] = 'not key'
 		res = post(self.test,self.url,self.data,self.content_type)
+		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_extra_payload(self):
 		self.data['extra item'] = 'something'
 		res = post(self.test,self.url,self.data,self.content_type)
+		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_min_payload(self):
 		del self.data['first name']
 		res = post(self.test,self.url,self.data,self.content_type)
+		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_activating_twice(self):
 		post(self.test,self.url,self.data,self.content_type)
 		res = post(self.test,self.url,self.data,self.content_type)
+		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,406)
 
 	def test_valid_activation_data(self):
 		res = post(self.test,self.url,self.data,self.content_type)
+		data = json.loads(res.get_data().decode('UTF-8'))
+		self.assertEqual(data,'')
 		self.assertEqual(res.status_code,200)
 
 if __name__ == '__main__':
