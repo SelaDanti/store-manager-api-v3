e2db4d75bb8ed616ce35da55f26a8580d2f3ecfe
SelaDanti <kwameasiago@gmail.com>
2018-11-02 07:28:00 +0300

    :new:[Feature #161413104] Set up heroku setting

--- instance/config.py
+++ instance/config.py
@@ -16,8 +16,17 @@ class TestConfig(Config):
 class DevelopmentConfig(Config):
 	DEBUG = True
 
+class DeployConfig(Config):
+	DEBUG = True
+	DATABASE_NAME = 'de2al00c9h6tl1'
+	PASSWORD = '22b586de11923087e5703b008bd521b34633db08c58088a2354ce78c426370df'
+	USER = 'mtonjwucrfsnwu'
+	HOST = 'ec2-184-73-222-192.compute-1.amazonaws.com'
+	PORT = 5432
+
 app_config = {
 	'testing':TestConfig,
-	'development':DevelopmentConfig
+	'development':DevelopmentConfig,
+	'deploy':DeployConfig
 }
 
