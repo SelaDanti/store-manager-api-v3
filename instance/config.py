0705690f9d0c5ffe5f84b9c523928b825e053cd8
SelaDanti <kwameasiago@gmail.com>
2018-11-13 18:17:55 +0300

    :bug: [Bug #161877389] Update heroku  config

--- instance/config.py
+++ instance/config.py
@@ -18,10 +18,10 @@ class DevelopmentConfig(Config):
 
 class DeployConfig(Config):
 	DEBUG = True
-	DATABASE_NAME = 'de2al00c9h6tl1'
-	PASSWORD = '22b586de11923087e5703b008bd521b34633db08c58088a2354ce78c426370df'
-	USER = 'mtonjwucrfsnwu'
-	HOST = 'ec2-184-73-222-192.compute-1.amazonaws.com'
+	DATABASE_NAME = 'd9pruu5aeui9mh'
+	PASSWORD = '3243698d2ab20a9615af086b9f693c8111ed716036da5bda43b9ba8b97a2c9b1'
+	USER = 'igwzrinpuwezvt'
+	HOST = 'ec2-107-20-193-206.compute-1.amazonaws.com'
 	PORT = 5432
 
 app_config = {
